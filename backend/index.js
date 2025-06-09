6faa9716a3c6e1ae9dabd1d8ff2c3a1113a70b53
JoaoPedroPess <joaopedropess03@gmail.com>
2025-06-09 19:35:26 -0300

    fix: remover comentarios

--- backend/index.js
+++ backend/index.js
@@ -5,15 +5,14 @@ const fs = require('fs');
 
 app.use(cors());
 
-// Lê o arquivo users.json
+
 const users = JSON.parse(fs.readFileSync('./users.json'));
 
-// Rota GET /usuarios
+
 app.get('/usuarios', (req, res) => {
   res.json(users);
 });
 
-// Rota GET /dados/:id
 app.get('/dados/:id', (req, res) => {
   const user = users.find(u => u.id === parseInt(req.params.id));
   if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
